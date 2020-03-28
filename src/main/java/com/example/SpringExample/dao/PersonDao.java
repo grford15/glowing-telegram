7ffce6d5096b8bf7aac1ce04b414cb35dc352987
Greg <g.rutherford10@gmail.com>
2020-03-28 13:06:05 +0000

    add routes & methods to PUT & DELETE people from DB

--- src/main/java/com/example/SpringExample/dao/PersonDao.java
+++ src/main/java/com/example/SpringExample/dao/PersonDao.java
@@ -3,6 +3,7 @@ package com.example.SpringExample.dao;
 import com.example.SpringExample.model.Person;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PersonDao {
@@ -15,5 +16,12 @@ public interface PersonDao {
     }
 
     List<Person> selectAllPeople();
+
+    Optional<Person> selectPersonById(UUID id);
+
+    int deletePersonById(UUID id);
+
+    int updatePersonByID(UUID id, Person person);
+
 
 }
